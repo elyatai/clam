febddbb22e3c43bee0f3366cb81db7f655687619
elyatai <35362971+elyatai@users.noreply.github.com>
2021-01-15 04:53:49 -0800

    create bot scaffold

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,14 @@
 module Main (main) where
 import Clam.Prelude
 
-import Calamity.Types
+import Calamity
+import Calamity.Cache.InMemory (runCacheInMemory)
+import Calamity.Commands
+import Calamity.Metrics.Noop (runMetricsNoop)
 import Dhall
+import qualified Di
+import DiPolysemy (info, runDiToIO)
+import Polysemy
 
 instance FromDhall Token where
   autoWith = fmap BotToken . autoWith
@@ -13,6 +19,15 @@ data Config = Config
     deriving anyclass FromDhall
 
 main ∷ IO ()
-main = do
+main = Di.new \di → do
   conf ← inputFile @Config auto "./config.dhall"
-  print (conf ^. #token)
+  res ← runFinal . embedToFinal
+    . runCacheInMemory . runMetricsNoop . runDiToIO di
+    . useConstantPrefix ";"
+    $ runBotIO (conf ^. #token) defaultIntents do
+      react @'ReadyEvt \rd → do
+        let u = rd ^. #user
+        info $ "Ready as " <> u ^. #username <> "#" <> u ^. #discriminator
+  whenJust res \(StartupError s) →
+    Di.runDiT di $ Di.alert $ "Startup error: " <> s
+
