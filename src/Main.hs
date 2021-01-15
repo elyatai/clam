7eef308dd6611da0f79158e9817c852269afcf28
elyatai <35362971+elyatai@users.noreply.github.com>
2021-01-15 07:04:46 -0800

    ping command, command prefix in config file

--- src/Main.hs
+++ src/Main.hs
@@ -5,7 +5,8 @@ import Calamity
 import Calamity.Cache.InMemory (runCacheInMemory)
 import Calamity.Commands
 import Calamity.Metrics.Noop (runMetricsNoop)
-import Dhall
+import qualified Data.Text.Lazy as L
+import Dhall hiding (void)
 import qualified Di
 import DiPolysemy (info, runDiToIO)
 import Polysemy
@@ -15,6 +16,7 @@ instance FromDhall Token where
 
 data Config = Config
   { token ∷ Token
+  , prefix ∷ L.Text
   } deriving Generic
     deriving anyclass FromDhall
 
@@ -23,11 +25,16 @@ main = Di.new \di → do
   conf ← inputFile @Config auto "./config.dhall"
   res ← runFinal . embedToFinal
     . runCacheInMemory . runMetricsNoop . runDiToIO di
-    . useConstantPrefix ";"
+    . useConstantPrefix (conf ^. #prefix)
     $ runBotIO (conf ^. #token) defaultIntents do
+
       react @'ReadyEvt \rd → do
         let u = rd ^. #user
         info $ "Ready as " <> u ^. #username <> "#" <> u ^. #discriminator
+
+      addCommands do
+        command @'[] "ping" \ctx →
+          void $ tell @Text (ctx ^. #message) "pong"
+
   whenJust res \(StartupError s) →
     Di.runDiT di $ Di.alert $ "Startup error: " <> s
-
