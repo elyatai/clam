8d7f0c188b9e2099da0c1c68842ca056ecd3d706
elyatai <35362971+elyatai@users.noreply.github.com>
2021-01-17 07:45:03 -0800

    bring in esqueleto and add sqlGetQ

--- src/Clam/Sql.hs
+++ src/Clam/Sql.hs
@@ -2,18 +2,18 @@
 module Clam.Sql
   ( Sql(..), SqlC
   , sqlGet, sqlGetUq, sqlSel, sqlCount, sqlHas
-  , sqlPut, sqlPut', sqlPutUq
-  , sqlDel, sqlDelUq
-  , sqlUpd
+  , sqlPut, sqlPut', sqlPutUq, sqlDel, sqlDelUq, sqlUpd
+  , sqlGetQ
   , runSqlConn, runSqlPool
-  , Sql.SqlBackend
+  , P.SqlBackend
   ) where
 
 import Clam.Prelude
 
 import Data.Pool (Pool)
 import Database.Persist
-import qualified Database.Persist.Sql as Sql
+import qualified Database.Persist.Sql as P
+import Database.Esqueleto.Internal.Internal as E
 
 type PRB r b = PersistRecordBackend r b
 
@@ -29,13 +29,15 @@ data Sql b m a where
     r → Sql b m (Either (Entity r) (Key r))
   SqlDel   ∷ PRB r b ⇒ Key r → Sql b m ()
   SqlDelUq ∷ PRB r b ⇒ Unique r → Sql b m ()
-  SqlUpd   ∷ PRB r b ⇒ Key r → [Update r] → Sql b m ()
+  SqlUpd   ∷ PRB r b ⇒ Key r → [P.Update r] → Sql b m ()
+
+  SqlGetQ  ∷ SqlSelect a r ⇒ SqlQuery a → Sql b m [r]
 
 makeSem ''Sql
 
 type SqlC backend =
   ( PersistStore backend, PersistUnique backend, PersistQuery backend
-  , BackendCompatible Sql.SqlBackend backend
+  , BackendCompatible P.SqlBackend backend
   )
 
 runSqlConn ∷ ∀ backend r a.
@@ -43,25 +45,27 @@ runSqlConn ∷ ∀ backend r a.
   backend → Sem (Sql backend ': r) a → Sem r a
 runSqlConn conn = transform go . reinterpret (embed . runSql1) where
   go ∷ ∀ m x. Embed (ReaderT backend IO) m x → Embed IO m x
-  go = Embed . flip Sql.runSqlConn conn . unEmbed
+  go = Embed . flip P.runSqlConn conn . unEmbed
 
 runSqlPool ∷ ∀ backend r a.
   (SqlC backend, Member (Embed IO) r) ⇒
   Pool backend → Sem (Sql backend ': r) a → Sem r a
 runSqlPool pool = transform go . reinterpret (embed . runSql1) where
   go ∷ ∀ m x. Embed (ReaderT backend IO) m x → Embed IO m x
-  go = Embed . flip Sql.runSqlPool pool . unEmbed
+  go = Embed . flip P.runSqlPool pool . unEmbed
 
 runSql1 ∷ SqlC backend ⇒ Sql backend m x → ReaderT backend IO x
 runSql1 = \case
   SqlGet k → get k
   SqlGetUq u → getBy u
   SqlSel fs → selectList fs []
-  SqlCount fs → fromIntegral <$> count fs
-  SqlHas fs → (/= 0) <$> count fs
+  SqlCount fs → fromIntegral <$> P.count fs
+  SqlHas fs → (/= 0) <$> P.count fs
   SqlPut x → insert x
   SqlPut' k x → insertKey k x
   SqlPutUq x → insertBy x
-  SqlDel k → delete k
+  SqlDel k → P.delete k
   SqlDelUq u → deleteBy u
-  SqlUpd k us → update k us
+  SqlUpd k us → P.update k us
+
+  SqlGetQ q → E.select q
