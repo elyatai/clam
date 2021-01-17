cf8e648204e621520d79b3c445f0d654cea36da3
elyatai <35362971+elyatai@users.noreply.github.com>
2021-01-16 19:22:27 -0800

    misc

--- src/Main.hs
+++ src/Main.hs
@@ -84,7 +84,7 @@ mkPermsCheck s p = buildCheck (s <> " permissions") \ctx →
   case ctx ^. #guild of
     Nothing → pure $ Just "Not in a server"
     Just g →
-      -- HACK for some reason ctx ^. #member is Nothing
+      -- HACK: for some reason ctx ^. #member is Nothing
       invoke (GetGuildMember g $ ctx ^. #user) >>= \case
         Left e → alert (show @Text e) $> Just "Something went wrong"
         Right m → pure $
