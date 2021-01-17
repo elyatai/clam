8dc0d3e5af799e3a467bc2f45f75cf485c0b1f70
elyatai <35362971+elyatai@users.noreply.github.com>
2021-01-17 04:37:20 -0800

    use `error` for logging

--- src/Clam/Prelude.hs
+++ src/Clam/Prelude.hs
@@ -1,14 +1,14 @@
 module Clam.Prelude (module M) where
 
 import Relude as M hiding
-  ( uncons
+  ( uncons, error
   , Reader, runReader, ask, asks, local
   , State, runState, get, put, gets, modify
   )
 import Control.Arrow as M hiding (first, second)
 import Control.Lens as M hiding ((??), rewrite, transform, Context)
-import DiPolysemy as M -- note: `error` conflicts with relude so omitted
-  (debug, info, notice, warning, alert, critical, emergency)
+import DiPolysemy as M
+  (debug, info, notice, warning, error, alert, critical, emergency)
 import Polysemy as M
 import Polysemy.Reader as M
 import Polysemy.Fail as M
