6686f22408f6344f5bd027977980eebe2b0c1b73
elyatai <35362971+elyatai@users.noreply.github.com>
2021-01-18 04:22:56 -0800

    needed a type application

--- src/Clam/Utils/Calamity.hs
+++ src/Clam/Utils/Calamity.hs
@@ -7,14 +7,14 @@ import Relude.Unsafe (fromJust)
 
 import Calamity hiding (Member)
 import Calamity.Commands (command, Context, DSLState)
-import Calamity.Commands.CommandUtils (CommandForParsers)
+import Calamity.Commands.CommandUtils (TypedCommandC, CommandForParsers)
 import Text.Emoji (emojiFromAlias)
 
 type Cmd r = Clam.BotC r ⇒ Sem (DSLState r) ()
 
 command_ ∷ ∀ ps r. TypedCommandC ps r ⇒
   Text → (Context → CommandForParsers ps r) → Cmd r
-command_ n = void . command n
+command_ n = void . command @ps n
 
 reactTo ∷ (Calamity.BotC r, HasID Channel a, HasID Message a) ⇒
   a → RawEmoji → Sem r (Either RestError ())
